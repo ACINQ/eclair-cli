7a10b0b71281f0ef5921be8f492c87dbceed3385
Mohit Kumar <madscientists1523@gmail.com>
2023-07-20 15:14:50 +0530

    Refractor commands.connect

--- src/nativeMain/kotlin/commands/connect.kt
+++ src/nativeMain/kotlin/commands/connect.kt
@@ -10,31 +10,24 @@ class ConnectCommand(
     private val resultWriter: IResultWriter,
     private val eclairClient: IEclairClient
 ) : Subcommand("connect", "Connect to another lightning node") {
-    private var password by option(
+    private val password by option(
         ArgType.String, shortName = "p", description = "Password for the Eclair API"
     ).required()
-    private var host by option(
+    private val host by option(
         ArgType.String, description = "Host URL for the Eclair API"
     ).default("http://localhost:8080")
-    private var uri by option(
-        ArgType.String, shortName = "u", description = "The URI in format 'nodeId@host:port'"
-    ).default("")
-    private var nodeId by option(
-        ArgType.String, shortName = "n", description = "The nodeId of the node you want to connect to"
-    ).default("")
-    private var manualHost by option(
-        ArgType.String, shortName = "m", description = "The IPv4 host address of the node (for manual connect)"
-    ).default("")
+    private val uri by argument(ArgType.String, description = "The URI in format 'nodeId@host:port'").optional()
+    private val nodeId by argument(ArgType.String, description = "The nodeId of the node you want to connect to").optional()
+    private val manualHost by argument(ArgType.String, description = "The IPv4 host address of the node").optional()
 
     override fun execute() {
         runBlocking {
             val result = when {
-                uri.isNotEmpty() -> eclairClient.connectUri(password, host, uri)
-                nodeId.isNotEmpty() && manualHost.isNotEmpty() -> eclairClient.connectManual(password, host, nodeId, manualHost)
-                nodeId.isNotEmpty() -> eclairClient.connectNodeId(password, host, nodeId)
-                else -> error("Please provide either uri or nodeId and host or nodeId only")
+                uri != null -> eclairClient.connectUri(password, host, uri!!)
+                nodeId != null && manualHost != null -> eclairClient.connectManual(password, host, nodeId!!, manualHost!!)
+                nodeId != null -> eclairClient.connectNodeId(password, host, nodeId!!)
+                else -> throw IllegalArgumentException("You must specify either a uri, or a nodeId and optional host")
             }
-
             result.fold(
                 onSuccess = {
                     resultWriter.writeSuccess("Connected to node")
