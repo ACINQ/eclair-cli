a0a73ebeee01658b0f9a915175d8d0e7aad017c8
Mohit Kumar <madscientists1523@gmail.com>
2023-07-20 15:40:31 +0530

    Update error handling for getinfo

--- src/nativeMain/kotlin/commands/GetInfo.kt
+++ src/nativeMain/kotlin/commands/GetInfo.kt
@@ -7,29 +7,24 @@ import kotlinx.coroutines.runBlocking
 
 @OptIn(ExperimentalCli::class)
 class GetInfoCommand(
-    private val resultWriter: IResultWriter,
-    private val eclairClient: IEclairClient
+    private val resultWriter: IResultWriter, private val eclairClient: IEclairClient
 ) : Subcommand(
     "getinfo",
     "Get information about this instance such as version, features, nodeId and current block height as seen by the eclair."
 ) {
     private var password by option(
-        ArgType.String,
-        shortName = "p",
-        description = "Password for the Eclair API"
+        ArgType.String, shortName = "p", description = "Password for the Eclair API"
     ).required()
     private var host by option(
-        ArgType.String,
-        shortName = "l",
-        description = "Host URL for the Eclair API"
+        ArgType.String, shortName = "l", description = "Host URL for the Eclair API"
     ).default("http://localhost:8080")
 
     override fun execute() = runBlocking {
         val infoResult = eclairClient.getInfo(password, host)
-        if (infoResult.isSuccess) {
+        infoResult.fold(onSuccess = {
             resultWriter.writeSuccess(infoResult.getOrThrow())
-        } else {
+        }, onFailure = {
             resultWriter.writeError("Error fetching information: ${infoResult.exceptionOrNull()?.message}")
-        }
+        })
     }
 }
