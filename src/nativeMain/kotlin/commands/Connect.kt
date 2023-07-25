126cfeb11e5bb75d908052c0b94090a06ae92f48
Mohit Kumar <madscientists1523@gmail.com>
2023-07-25 16:36:53 +0530

    Fix description of uri

--- src/nativeMain/kotlin/commands/Connect.kt
+++ src/nativeMain/kotlin/commands/Connect.kt
@@ -17,7 +17,7 @@ class ConnectCommand(
     "connect",
     "Connect to another lightning node. This will perform a connection but no channels will be opened. "
 ) {
-    private val uri by argument(ArgType.String, description = " This API does not require a target address. Instead, eclair will use one of the addresses published by the remote peer in its node_announcement messages.")
+    private val uri by argument(ArgType.String, description = "If the uri to the target node is not provided, eclair will use one of the addresses published by the remote peer in its node_announcement messages if it can be found.")
     override fun execute() = runBlocking {
         val eclairClient = eclairClientBuilder.build(host, password)
         val result = eclairClient.connect(uri)
--- src/nativeTest/kotlin/mocks/EclairClientMocks.kt
+++ src/nativeTest/kotlin/mocks/EclairClientMocks.kt
@@ -17,7 +17,7 @@ class DummyEclairClient(
     companion object {
         val validGetInfoResponse = """{"version":"0.9.0","nodeId":"03e319aa4ecc7a89fb8b3feb6efe9075864b91048bff5bef14efd55a69760ddf17","alias":"alice","color":"#49daaa","features":{"activated":{"var_onion_optin":"mandatory","option_static_remotekey":"optional"},"unknown":[151,178]},"chainHash":"06226e46111a0b59caaf126043eb5bbf28c34f3a5e332a1fc7b2b73cf188910f","network":"regtest","blockHeight":107,"publicAddresses":[],"instanceId":"be74bd9a-fc54-4f24-bc41-0477c9ce2fb4"}"""
         val invalidGetInfoResponse = """{"version":42}"""
-        val validConnectResponse = "Connected successfully"
+        val validConnectResponse = "connected"
     }
 }
 
