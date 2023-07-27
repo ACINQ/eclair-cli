e85f2d64faa1dfb7700afe16457f82955505c7a9
Mohit Kumar <madscientists1523@gmail.com>
2023-07-27 23:28:05 +0530

    Add unit test for ConnectCommand

--- src/nativeMain/kotlin/commands/Base.kt
+++ src/nativeMain/kotlin/commands/Base.kt
@@ -4,6 +4,6 @@ import kotlinx.cli.*
 
 @OptIn(ExperimentalCli::class)
 abstract class BaseCommand(commandName: String, commandDescription: String): Subcommand(commandName, commandDescription) {
-    protected val password by option(ArgType.String, shortName = "p", description = "Password for the Eclair API (can be found in eclair.conf)").required()
-    protected val host by option(ArgType.String, description = "Host URL for the Eclair API (can be found in eclair.conf)").default("http://localhost:8080")
+    var password by option(ArgType.String, shortName = "p", description = "Password for the Eclair API (can be found in eclair.conf)").required()
+    var host by option(ArgType.String, description = "Host URL for the Eclair API (can be found in eclair.conf)").default("http://localhost:8080")
 }
